e6304deb4c8ccc57665b628c007fed60e1abdd2f
sakura <2777679537@qq.com>
2022-11-29 13:51:40 +0800

    init

--- src/views/business/check/component/check-main-detail.vue
+++ src/views/business/check/component/check-main-detail.vue
@@ -7,7 +7,7 @@
       </div>
       <div class="content-wrapper">
         <div class="title">症状:</div>
-        <div>{{data.putUp}}</div>
+        <div v-html="putUp"></div>
       </div>
       <div class="content-wrapper">
         <div class="title">药方:</div>
@@ -25,6 +25,12 @@
   export default {
     name: "check-main-detail",
     props: {
+      high: {
+        type: Array,
+        default() {
+          return [];
+        }
+      },
       data: {
         type: Object,
         default() {
@@ -36,6 +42,26 @@
       return {
         height: 0
       }
+    },
+    computed: {
+      putUp() {
+        let highNames = this.high.map(item => item.value);
+        if (this.validatenull(this.data)) {
+          return "";
+        }
+
+        let putUps = this.data.putUp.split("，");
+        let html = "";
+        for(let i = 0; i<putUps.length; i++) {
+          let item = putUps[i];
+          let index = highNames.indexOf(item);
+          html += index === -1 ? `<span>${item}</span>` : `<span style="color: #F56C6C;">${item}</span>`
+          if (i < putUps.length - 1) {
+            html += "，";
+          }
+        }
+        return html;
+      },
     },
     methods: {
       setHeight() {
